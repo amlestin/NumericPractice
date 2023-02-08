c694880da14fcaf42151c0091ac0fa7a894b11df
Arnold Lestin <amlestin@gmail.com>
2023-02-08 16:33:04 -0600

    move instructions to function

--- TypingTest.java
+++ TypingTest.java
@@ -4,21 +4,26 @@ import java.util.Random;
 public class TypingTest
 {
    final static int MAX_ERRORS = 4;
-   public static void main(String[] args)
-   {
-      Scanner keyboard = new Scanner(System.in);
+
+   public static void print_instructions() {
       System.out.println("This game will allow you to demonstrate and develop your number typing skills! \n");
       System.out.println("A random number (0-9) will appear, and you will have to enter it in a specified amount of time. \n");
       System.out.println("If you fail to enter the correct number three times in a row, you lose. Try to get the highest score!");
-      
       System.out.println("======================================================================================================");
-      
+   }
+
+   public static void main(String[] args)
+   {
+      Scanner keyboard = new Scanner(System.in);
+
+      print_instructions();
+
       boolean wait = true;
       while(wait)
       {
          System.out.println("Are you ready? Enter \"Y\" to continue.");
          String input = keyboard.nextLine();
-         wait = input.equalsIgnoreCase("y") ? false : true;
+         wait = !input.equalsIgnoreCase("y");
       }
       
       int score = 0;
@@ -46,7 +51,7 @@ public class TypingTest
             row[answer]--;
          }
          
-         alive = (errors < MAX_ERRORS) ? true : false;
+         alive = errors < MAX_ERRORS;
       }
          
       System.out.println("Your correctly entered: " + score + " numbers!");
