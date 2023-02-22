708c4e3a30cf5e4d370b50b05e1694e9a70031aa
Arnold Lestin <amlestin@gmail.com>
2023-02-22 15:05:24 -0600

    added input validation

--- src/main/java/typingtest/TypingTest.java
+++ src/main/java/typingtest/TypingTest.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 import java.util.Random;
 public class TypingTest
 {
+      static int DEFAULT_MAX_ERRORS = 4;
 
       public static void main(String[] args)
    {
       Scanner keyboard = new Scanner(System.in);
       Random generator = new Random();
-
-      int max_errors = 4;
+      int max_errors;
 
       System.out.println("What is your maximum number of errors? ");
       int user_max_errors = GameManager.getUserInput(keyboard);
-      // TODO: Error check response
-      max_errors = user_max_errors;
+      if (user_max_errors <= 0)
+         max_errors = DEFAULT_MAX_ERRORS;
+      else
+         max_errors = user_max_errors;
 
       GameManager gm = new GameManager(max_errors);
 
