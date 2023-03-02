fd3c96d910b962711d096af4475cdca99c495fc2
Arnold Lestin <amlestin@gmail.com>
2023-03-01 20:19:03 -0600

    refactored to add training mode

--- src/main/java/typingtest/TypingTest.java
+++ src/main/java/typingtest/TypingTest.java
@@ -5,6 +5,7 @@ public class TypingTest
       public static void main(String[] args)
    {
       GameManager gm = new GameManager();
+      // TODO: ask if this will be training or a game/test
       gm.runGame();
    }
 }
